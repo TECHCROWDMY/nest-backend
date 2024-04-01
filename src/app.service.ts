e3a12bfba4438ee9d0461194ff348100b1385563
Thilak A/L Sundaram <thilak014@gmail.com>
2024-04-01 22:04:31 +0800

    Added Post method

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,24 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      // status: 'success',
+      // message: 'Hello World!',
+      // data: null,
+      assistant: {
+        firstMessage: 'Hey Ryan, how are you?',
+        model: {
+          provider: 'openai',
+          model: 'gpt-3.5-turbo',
+          messages: [
+            {
+              role: 'system',
+              content: "You're Ryan's assistant...",
+            },
+          ],
+        },
+      },
+    };
   }
 }
